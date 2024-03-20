81c47d64f8bafbc3954f921b9d7f68276e2474a1
Kabeer Mumuni <Kmumuni@Orangedrugs.local>
2024-03-20 15:02:56 +0100

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,58 @@
+import { Controller, Get, Delete, Put, Post, Param, Body, HttpCode, ParseUUIDPipe, ParseEnumPipe } from "@nestjs/common"
+import { ReportType } from "./data"
+import { AppService } from "./app.service";
+import { CreateReportDto, UpdateReportDto } from "./dtos/report.dto"
+
+@Controller('report/:type')
+export class AppController {
+
+  constructor(
+    private readonly appService: AppService
+  ) { }
+
+  @Get()
+  getAllReports(@Param('type', new ParseEnumPipe(ReportType)) type: string) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSES;
+    return this.appService.getAllReportsService(reportType)
+  }
+
+  @Get(':id')
+  getReportById(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string
+  ) {
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSES;
+    return this.appService.getReportById(reportType, id)
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: CreateReportDto,
+    @Param('type', new ParseEnumPipe(ReportType)) type: string) {
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSES
+    return this.appService.createReport(reportType, { amount, source })
+
+  }
+
+
+  @Put(':id')
+  updateReport(
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdateReportDto
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSES;
+
+    return this.appService.updateReport(reportType, id, body)
+
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id', ParseUUIDPipe) id: string
+  ) {
+    return this.appService.deleteReport
+
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@nestjs/common";
+import { ReportType, data } from "./data";
+import { v4 as uuid } from "uuid"
+
+
+interface Report { amount: number, source: string }
+
+interface UpdateReport { amount?: number, source?: string }
+
+@Injectable()
+export class AppService {
+
+  getAllReportsService(type: ReportType) {
+    return data.report.filter((report) => report.type === type)
+  }
+
+  getReportById(type: ReportType, id: string) {
+    return data.report.filter((report) => report.type === type).find((report) => report.id === id)
+  }
+
+  createReport(type: ReportType, { amount, source }: Report) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type
+    }
+    data.report.push(newReport)
+    return newReport
+  }
+
+  updateReport(type: ReportType, id: string, body: UpdateReport) {
+    const updatedReport = data.report.filter((report) => report.type === type).find((report) => report.id === id);
+
+    if (!updatedReport) return;
+
+    const reportIndex = data.report.findIndex((report) => report.id === updatedReport.id)
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+      updated_at: new Date()
+    }
+
+    return data.report[reportIndex]
+
+  }
+
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id)
+
+    if (reportIndex === -1) return "The said Data is not Found";
+
+    data.report.splice(reportIndex, 1);
+
+    return 'deleted'
+  }
+} 
